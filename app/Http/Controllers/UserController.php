b4671d564f27b47f7464bab8be64d842470968ca
Adrian Umbao <umbaoadrian3@gmail.com>
2025-03-03 23:13:06 +0800

    rules

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,4 +29,16 @@ class UserController extends Controller
         $users = User::all();
         return $this->successResponse($users);
     }
+    public function add(Request $request ){
+        $rules = [
+            'username' => 'required|max:20',
+            'password' => 'required|max:20'
+        ];
+
+        $this->validate($request, $rules);
+        $user = user::create($request->all());
+        return $this->successResponse($user, Response::HTTP_CREATED);
+
+
+    }
 }
