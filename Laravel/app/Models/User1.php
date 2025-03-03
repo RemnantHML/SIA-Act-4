49fd5a5b8ece26c61bd12b1c61c343b4d18c90a1
Adrian Umbao <umbaoadrian3@gmail.com>
2025-03-03 19:45:12 +0800

    model underscore

--- Laravel/app/Models/User1.php
+++ Laravel/app/Models/User1.php
@@ -5,7 +5,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
  class User extends Model{
-     protected $table = 'tbluser';
+     protected $table = 'tbl_user';
 // column sa table
      protected $fillable = [
      'username', 'password'
