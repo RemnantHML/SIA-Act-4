2dc515a542b84701d957ae0707c2423fead337bf
Adrian Umbao <umbaoadrian3@gmail.com>
2025-03-03 19:08:21 +0800

    old

--- Laravel/app/Models/User_old.php
+++ Laravel/app/Models/User_old.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User_old extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable, HasFactory;
 
