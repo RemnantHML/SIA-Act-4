90879215a4d3ae8e8f75fcbc7d13155ec3948d73
Adrian Umbao <umbaoadrian3@gmail.com>
2025-03-08 14:47:04 +0800

    post

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->get('/users', 'UserController@getUsers');
+$router->post('/users', 'UserController@getUsers');
 $router->group(['prefix' => 'api'], function () use ($router) {
 $router->get('/users', 'UserController@getUsers'); // Get all users
 $router->get('/users/update', 'UserController@updateUser');
