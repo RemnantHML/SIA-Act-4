1b05f68dfb8c016520d966977b836ba31b8cfaaf
Adrian Umbao <umbaoadrian3@gmail.com>
2025-03-08 14:46:02 +0800

    please work

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ $router->get('/', function () use ($router) {
 });
 $router->get('/users', 'UserController@getUsers');
 $router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('/users', 'UserController@getUsers'); // Get all users
+$router->get('/users', 'UserController@getUsers'); // Get all users
 $router->get('/users/update', 'UserController@updateUser');
 });
