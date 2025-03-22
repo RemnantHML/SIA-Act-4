9c518afd7bb60a5ace8079b5b8dc765a48402b6b
Adrian Umbao <umbaoadrian3@gmail.com>
2025-03-23 05:41:42 +0800

    api??

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
+
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->post('/users', 'UserController@getUsers');
+
 $router->group(['prefix' => 'api'], function () use ($router) {
-$router->get('/users', 'UserController@getUsers'); // Get all users
-$router->post('/users/update', 'UserController@updateUser');
+    $router->get('/users', 'UserController@getUsers'); // Get all users
+    $router->post('/users/update', 'UserController@updateUser'); // Update user
 });
