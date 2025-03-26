be9336275616cba6280398d8d685268bd471e708
Adrian Umbao <umbaoadrian3@gmail.com>
2025-03-26 18:35:08 +0800

    yes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,6 +41,7 @@ Class UserController extends Controller {
         $rules = [
             'username' => 'required|max:20',
             'password' => 'required|max:20',
+            'gender' => 'required|in:Male,Female',
         ];
 
         $this->validate($request,$rules);
